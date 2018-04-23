a3b95726404d4eb309e8bfae12ac2d6c56979b6e
chester <291164091@qq.com>
2018-04-23 22:56:34 +0800

    try methods

--- src/day5/example/main.go
+++ src/day5/example/main.go
@@ -8,11 +8,16 @@ type student struct {
 	score float64
 }
 
+func (ex student) init()  {
+	fmt.Println(ex.score)
+}
+
 func main()  {
 	var stu student
 	stu.name = "Gavin"
 	stu.age = 18
 	stu.score = 65.0
+	stu.init()
 	fmt.Printf("name: %p\n" ,&stu.name)
 	fmt.Printf("age: %p\n" ,&stu.age)
 	fmt.Printf("score: %p\n" ,&stu.score)
